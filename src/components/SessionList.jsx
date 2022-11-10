854aff613b3a1daf6330ad3ca1ee47c7d5386c8d
jameseidson <eidsonje@icloud.com>
2022-11-10 14:03:37 -0600

    implemented searching

--- src/components/SessionList.jsx
+++ src/components/SessionList.jsx
@@ -1,21 +1,21 @@
 import "./SessionList.css";
 
 import Form from "react-bootstrap/Form";
-// import FuzzySearch from "fuzzy-search";
+import FuzzySearch from "fuzzy-search";
 import InputGroup from "react-bootstrap/InputGroup";
 // import { RideInfo } from "./RideInfo";
 import { Search } from "react-bootstrap-icons";
 import { Session } from "./Session";
 
 // import { useDbData } from "../utilities/firebase";
-// import { useState } from "react";
+import { useState } from "react";
 
 // import { BookRide } from "./BookRide";
 
 export const SessionList = ({ sessions, courses, user }) => {
   // const [show, setShow] = useState(false);
   // const [selectedRide, setSelectedRide] = useState();
-  // const [searchstr, setSearch] = useState("");
+  const [searchstr, setSearch] = useState("");
 
   // const handleClose = () => setShow(false);
   // const handleShow = (ride) => {
@@ -23,25 +23,17 @@ export const SessionList = ({ sessions, courses, user }) => {
   //   setSelectedRide(ride);
   // };
 
-  // const ridesValues =
-  //   rides == null ? [] : Object.entries(rides).map(([k, v]) => v);
+  const searcher = new FuzzySearch(
+    null ? [] : Object.entries(sessions).map(([k, v]) => v),
+    ["course", "location"],
+    {
+      caseSensitive: false,
+    }
+  );
 
-  // const searcher = new FuzzySearch(
-  //   ridesValues,
-  //   [
-  //     "destination.city",
-  //     "destination.state",
-  //     "destination.street_address",
-  //     "destination.zip",
-  //   ],
-  //   {
-  //     caseSensitive: false,
-  //   }
-  // );
-
-  // const filteredRides = () => {
-  //   return searchstr == "" ? rides : searcher.search(searchstr);
-  // };
+  const searchResults = () => {
+    return searchstr == "" ? sessions : searcher.search(searchstr);
+  };
   return (
     <div>
       <div className="SearchBar">
@@ -51,16 +43,12 @@ export const SessionList = ({ sessions, courses, user }) => {
           </InputGroup.Text>
           <Form.Control
             placeholder="Search for courses"
-            // onChange={(change) => setSearch(change.target.value)}
+            onChange={(change) => setSearch(change.target.value)}
           />
         </InputGroup>
       </div>
       <div className="mt-2">
-        {/* {Object.entries(filteredRides()).map(([id, ride]) => {
-          ride.key = id;
-          return <Ride user={user} key={id} ride={ride} handleShow={handleShow} />;
-        })} */}
-        {Object.entries(sessions).map(([id, session]) => {
+        {Object.entries(searchResults()).map(([id, session]) => {
           return (
             <Session
               key={id}
