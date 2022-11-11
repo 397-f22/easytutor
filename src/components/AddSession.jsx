3882f3bfca1c07daa6d8fdabf47d3812590304d2
Si Woo <siwoo2000@gmail.com>
2022-11-10 22:19:56 -0600

    credit logic

--- src/components/AddSession.jsx
+++ src/components/AddSession.jsx
@@ -1,3 +1,4 @@
+import Alert from "react-bootstrap/Alert";
 import { Form } from "react-bootstrap";
 import { addSession } from "../utilities/firebase";
 import { useNavigate } from "react-router-dom";
@@ -28,57 +29,74 @@ const ButtonBar = ({ disabled }) => {
 export const AddSession = ({ courses, user }) => {
   const navigate = useNavigate();
   const [selCourse, setSelCourse] = useState("");
+  // const [creditConflict, setCreditConflict] = useState(false);
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
     const formData = evt.target;
-    const date = new Date(`${formData.date.value}T${formData.time.value}:00`);
-    addSession(
-      selCourse,
-      date,
-      formData.duration.value,
-      formData.location.value,
-      user.uid,
-      user.credits - Math.ceil(formData.duration.value) * 10
-    );
-    navigate("/teach");
+    const newCredit = Math.ceil(formData.duration.value) * 10;
+
+    if (user.credits < newCredit) {
+      alert("Not enough credits");
+      // setCreditConflict(true);
+    } else {
+      // setCreditConflict(false);
+      const date = new Date(`${formData.date.value}T${formData.time.value}:00`);
+      addSession(
+        selCourse,
+        date,
+        formData.duration.value,
+        formData.location.value,
+        user.uid,
+        user.credits - newCredit
+      );
+      navigate("/teach");
+    }
   };
 
   return (
-    <Form className="p-3" onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" controlId="course">
-        <Form.Label>Course</Form.Label>
-        <Form.Select onChange={(evt) => setSelCourse(evt.target.value)}>
-          <option>Select Your Course</option>
-          {Object.entries(courses).map(([id, course]) => (
-            <option key={id} value={id}>
-              {course}
-            </option>
-          ))}
-        </Form.Select>
-      </Form.Group>
+    <>
+      {/* {creditConflict && (
+        <Alert variant="danger">You don't have enough credits!</Alert>
+      )} */}
+      <Form className="p-3" onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" controlId="course">
+          <Form.Label>Course</Form.Label>
+          <Form.Select
+            onChange={(evt) => setSelCourse(evt.target.value)}
+            required
+          >
+            <option>Select Your Course</option>
+            {Object.entries(courses).map(([id, course]) => (
+              <option key={id} value={id}>
+                {course}
+              </option>
+            ))}
+          </Form.Select>
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="location">
-        <Form.Label>Location</Form.Label>
-        <Form.Control type="text" />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="location">
+          <Form.Label>Location</Form.Label>
+          <Form.Control type="text" required />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="date">
-        <Form.Label>Date</Form.Label>
-        <Form.Control type="date" />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="date">
+          <Form.Label>Date</Form.Label>
+          <Form.Control type="date" required />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="time">
-        <Form.Label>Start Time</Form.Label>
-        <Form.Control type="time" />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="time">
+          <Form.Label>Start Time</Form.Label>
+          <Form.Control type="time" required />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="duration">
-        <Form.Label>Hours</Form.Label>
-        <Form.Control type="number" />
-      </Form.Group>
+        <Form.Group className="mb-3" controlId="duration">
+          <Form.Label>Hours</Form.Label>
+          <Form.Control type="number" required />
+        </Form.Group>
 
-      <ButtonBar />
-    </Form>
+        <ButtonBar />
+      </Form>
+    </>
   );
 };
